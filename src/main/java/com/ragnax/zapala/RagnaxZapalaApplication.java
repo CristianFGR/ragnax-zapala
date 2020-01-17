1f3ce572c9f20f796fdfd25a424e4719eb7e7e71
juliocornejo <elimino.gente.por.dinero@gmail.com>
2020-01-16 23:12:01 -0300

    first commit

--- src/main/java/com/ragnax/zapala/RagnaxZapalaApplication.java
+++ src/main/java/com/ragnax/zapala/RagnaxZapalaApplication.java
@@ -3,10 +3,11 @@ package com.ragnax.zapala;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EntityScan({"com.ragnax.domain.zapala.modelo"})
 public class RagnaxZapalaApplication {
 
